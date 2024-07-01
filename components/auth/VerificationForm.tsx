bb6d062517ba246bbe0a4cc5acb87901b1a686f2
GeraltWang <wgeralt@outlook.com>
2024-07-01 13:16:03 +0800

    feat: ✨ email verification

--- components/auth/VerificationForm.tsx
+++ components/auth/VerificationForm.tsx
@@ -1,16 +1,39 @@
 'use client'
-import { useCallback, useEffect } from 'react'
-import CardWrapper from '../CardWrapper'
+import { useCallback, useEffect, useState } from 'react'
+import CardWrapper from '@/components/CardWrapper'
+import FormError from '@/components/auth/FormError'
+import FormSuccess from '@/components/auth/FormSuccess'
 import { BeatLoader } from 'react-spinners'
 import { useSearchParams } from 'next/navigation'
+import { newVerification } from '@/lib/actions/new-verification.action'
 
 const VerificationForm = () => {
 	const searchParams = useSearchParams()
 	const token = searchParams.get('token')
 
+	const [error, setError] = useState<string | undefined>('')
+
+	const [success, setSuccess] = useState<string | undefined>('')
+
 	const onSubmit = useCallback(() => {
-		console.log(token)
-	}, [token])
+		if (success || error) {
+			return
+		}
+
+		if (!token) {
+			setError('Missing token')
+			return
+		}
+
+		newVerification(token)
+			.then(data => {
+				data?.error && setError(data.error)
+				data?.message && setSuccess(data.message)
+			})
+			.catch(() => {
+				setError('An error occurred')
+			})
+	}, [success, error, token])
 
 	useEffect(() => {
 		onSubmit()
@@ -19,7 +42,9 @@ const VerificationForm = () => {
 	return (
 		<CardWrapper headerLabel='Confirm your verification' backButtonLabel='Back to sign in' backButtonHref='/sign-in'>
 			<div className='flex-center w-full'>
-				<BeatLoader />
+				{!success && !error && <BeatLoader />}
+				{!success && <FormError message={error} />}
+				{!error && <FormSuccess message={success} />}
 			</div>
 		</CardWrapper>
 	)
---
+++ lib/actions/new-verification.action.ts
@@ -0,0 +1,55 @@
+'use server'
+import prisma from '@/prisma/client'
+import { handleError } from '@/lib/utils'
+import { getUserByEmail } from '@/lib/actions/user.action'
+import { getVerificationToKenByToken } from '@/lib/actions/verification-token.action'
+
+export const newVerification = async (token: string) => {
+	try {
+		const existingToken = await getVerificationToKenByToken(token)
+
+		if (!existingToken) {
+			return {
+				error: 'Token does not exist!',
+			}
+		}
+
+		const hasExpired = new Date(existingToken.expires) < new Date()
+
+		if (hasExpired) {
+			return {
+				error: 'Token has expired!',
+			}
+		}
+
+		const existingUser = await getUserByEmail(existingToken.email)
+
+		if (!existingUser) {
+			return {
+				error: 'User with this email does not exist!',
+			}
+		}
+
+		await prisma.user.update({
+			where: {
+				id: existingUser.id,
+			},
+			data: {
+				emailVerified: new Date(),
+				email: existingToken.email,
+			},
+		})
+
+		await prisma.verificationToken.delete({
+			where: {
+				id: existingToken.id,
+			},
+		})
+
+		return {
+			message: 'Email verified!',
+		}
+	} catch (error) {
+		handleError(error)
+	}
+}
