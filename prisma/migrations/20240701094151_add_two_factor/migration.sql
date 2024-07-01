db52cb65deaaacb3a5183d0a2a90f04db789736d
GeraltWang <wgeralt@outlook.com>
2024-07-01 17:42:46 +0800

    feat: ✨ init TwoFactor Table

---
+++ prisma/migrations/20240701094151_add_two_factor/migration.sql
@@ -0,0 +1,26 @@
+-- AlterTable
+ALTER TABLE `users` ADD COLUMN `isTwoFactorEnabled` BOOLEAN NOT NULL DEFAULT false;
+
+-- CreateTable
+CREATE TABLE `TwoFactorConfirmation` (
+    `id` VARCHAR(191) NOT NULL,
+    `userId` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `TwoFactorConfirmation_userId_key`(`userId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `TwoFactorToken` (
+    `id` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `token` VARCHAR(191) NOT NULL,
+    `expires` DATETIME(3) NOT NULL,
+
+    UNIQUE INDEX `TwoFactorToken_token_key`(`token`),
+    UNIQUE INDEX `TwoFactorToken_email_token_key`(`email`, `token`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `TwoFactorConfirmation` ADD CONSTRAINT `TwoFactorConfirmation_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
