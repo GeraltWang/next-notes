f7112698f72e012fb5773cb4f3f422f3c6fca7b7
GeraltWang <wgeralt@outlook.com>
2024-07-02 13:38:54 +0800

    refactor: ♻️ routes

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,10 +1,10 @@
 import { Resend } from 'resend'
-import { PASSWORD_RESET_ROOT, VERIFICATION_ROOT } from '@/routes'
+import { PASSWORD_RESET_ROUTE, VERIFICATION_ROUTE } from '@/routes'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 export const sendVerificationEmail = async (email: string, token: string) => {
-	const confirmLink = `http://localhost:3000${VERIFICATION_ROOT}?token=${token}`
+	const confirmLink = `http://localhost:3000${VERIFICATION_ROUTE}?token=${token}`
 
 	await resend.emails.send({
 		from: 'onboarding@resend.dev',
@@ -17,7 +17,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 }
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-	const resetLink = `http://localhost:3000${PASSWORD_RESET_ROOT}?token=${token}`
+	const resetLink = `http://localhost:3000${PASSWORD_RESET_ROUTE}?token=${token}`
 
 	await resend.emails.send({
 		from: 'onboarding@resend.dev',
--- routes.ts
+++ routes.ts
@@ -5,28 +5,38 @@
 export const DEFAULT_LOGIN_REDIRECT = '/'
 
 /**
+ * 登录路由
+ */
+export const SIGN_IN_ROUTE = '/sign-in'
+
+/**
+ * 注册路由
+ */
+export const SIGN_UP_ROUTE = '/sign-up'
+
+/**
  * 电子邮件验证路由
  */
-export const VERIFICATION_ROOT = '/new-verification'
+export const VERIFICATION_ROUTE = '/new-verification'
 
 /**
  * 重置密码路由
  */
-export const PASSWORD_RESET_ROOT = '/new-password'
+export const PASSWORD_RESET_ROUTE = '/new-password'
 
 /**
  * 公共访问路由
  * 无需鉴权
  * @type {string[]}
  */
-export const publicRoutes = ['/', VERIFICATION_ROOT, '/note/\\d+']
+export const publicRoutes = ['/', VERIFICATION_ROUTE, '/note/\\d+']
 
 /**
  * 授权路由
  * 用于登录/注册
  * @type {string[]}
  */
-export const authRoutes = ['/sign-in', '/sign-up', '/reset', PASSWORD_RESET_ROOT, '/error']
+export const authRoutes = [SIGN_IN_ROUTE, SIGN_UP_ROUTE, '/reset', PASSWORD_RESET_ROUTE, '/error']
 
 /**
  * API 授权路由前缀
