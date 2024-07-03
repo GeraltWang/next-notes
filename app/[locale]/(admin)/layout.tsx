3e13cd0fd930ab3bc8a5b2e55195f16d31f853b8
GeraltWang <wgeralt@outlook.com>
2024-07-03 14:17:49 +0800

    refactor: ♻️ remove unnecessary code

--- app/[locale]/(admin)/layout.tsx
+++ app/[locale]/(admin)/layout.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import Sidebar from '@/components/Sidebar'
-import { getMessages } from 'next-intl/server'
-import { NextIntlClientProvider } from 'next-intl'
 import Header from '@/components/Header'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
 	title: 'Create Next App',
@@ -14,26 +9,17 @@ export const metadata: Metadata = {
 
 export default async function RootLayout({
 	children,
-	params: { locale },
 }: Readonly<{
 	children: React.ReactNode
 	params: { locale: string }
 }>) {
-	const messages = await getMessages()
-
 	return (
-		<html lang={locale}>
-			<body className={inter.className}>
-				<NextIntlClientProvider messages={messages}>
-					<div className='w-full h-screen flex flex-col'>
-						<Header />
-						<div className='main'>
-							<Sidebar />
-							<section className='h-full w-full flex justify-center items-center'>{children}</section>
-						</div>
-					</div>
-				</NextIntlClientProvider>
-			</body>
-		</html>
+		<div className='w-full h-screen flex flex-col'>
+			<Header />
+			<div className='main'>
+				<Sidebar />
+				<section className='h-full w-full flex justify-center items-center'>{children}</section>
+			</div>
+		</div>
 	)
 }
