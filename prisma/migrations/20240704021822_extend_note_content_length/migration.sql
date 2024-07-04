e55ce0d0e820fb62e956ca222ad18ecb99bc969e
GeraltWang <wgeralt@outlook.com>
2024-07-04 10:26:11 +0800

    refactor: ♻️ note content text length

---
+++ prisma/migrations/20240704021822_extend_note_content_length/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE `notes` MODIFY `content` TEXT NOT NULL;
--- schema/note.ts
+++ schema/note.ts
@@ -2,5 +2,5 @@ import { z } from 'zod'
 
 export const NoteSchema = z.object({
 	title: z.string().min(1, '请填写标题'),
-	content: z.string().min(1, '请填写内容').max(2000, '字数最多 2000'),
+	content: z.string().min(1, '请填写内容'),
 })
