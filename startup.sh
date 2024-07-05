af0f55887b225b8c2e92412de1771bb5d9a805e7
GeraltWang <wgeralt@outlook.com>
2024-07-05 16:51:16 +0800

    ci: 👷 ci file

--- startup.sh
+++ startup.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-MIGRATION_STATUS=$(npx prisma migrate status)
+MIGRATION_STATUS=$(pnpm dlx prisma migrate status)
 
 if echo "$MIGRATION_STATUS" | grep -q "Database schema is up to date"; then
     echo "No migrations needed."
 else
     echo "Running migrations..."
-    npx prisma migrate deploy
+    pnpm dlx prisma migrate deploy
 fi
 
 node server.js
