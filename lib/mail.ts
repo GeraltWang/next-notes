924822d85717f9af4ca4e55481b3691381f13944
GeraltWang <wgeralt@outlook.com>
2024-07-05 14:30:32 +0800

    ci: 👷 NEXT_PUBLIC_APP_URL

--- lib/mail.ts
+++ lib/mail.ts
@@ -3,8 +3,10 @@ import { PASSWORD_RESET_ROUTE, VERIFICATION_ROUTE } from '@/routes'
 
 const resend = new Resend(process.env.RESEND_API_KEY)
 
+const domain = process.env.NEXT_PUBLIC_APP_URL
+
 export const sendVerificationEmail = async (email: string, token: string) => {
-  const confirmLink = `http://localhost:3000${VERIFICATION_ROUTE}?token=${token}`
+  const confirmLink = `${domain}${VERIFICATION_ROUTE}?token=${token}`
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
@@ -17,7 +19,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 }
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
-  const resetLink = `http://localhost:3000${PASSWORD_RESET_ROUTE}?token=${token}`
+  const resetLink = `${domain}${PASSWORD_RESET_ROUTE}?token=${token}`
 
   await resend.emails.send({
     from: 'onboarding@resend.dev',
@@ -35,7 +37,7 @@ export const sendTwoFactorEmail = async (email: string, token: string) => {
     to: email,
     subject: '2FA code',
     html: `
-      <p>Your 2FA code: ${token}</p>
+      <p>Your 2FA code is: ${token}</p>
     `
   })
 }
