0ca19a5a889f74dde1ae47a3e0b422ac8c3cbfe9
GeraldWang <wgeralt@outlook.com>
2024-07-07 15:42:15 +0800

    Revert "refactor: ♻️ middleware"
    This reverts commit b226ca0fc05f5c7954ff6dd6b4b6668912f3e707.

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { defaultLocale, locales } from '@/config'
 import { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes, SIGN_IN_ROUTE } from '@/routes'
 import NextAuth from 'next-auth'
 import createMiddleware from 'next-intl/middleware'
-import { NextRequest } from 'next/server'
 
 const { auth } = NextAuth(authConfig)
 
@@ -21,94 +20,46 @@ const routeTester = (routes: string[], path: string) => {
   return Regex.test(path)
 }
 
-// export default auth((req) => {
-//   console.log('🚀 ~ authMiddleware ~ ROUTE:', req.nextUrl.pathname)
-//   const { nextUrl } = req
-
-//   // 登录状态
-//   const isLoggedIn = !!req.auth
-//   console.log('🚀 ~ authMiddleware ~ isLoggedIn:', isLoggedIn)
-
-//   // 是否是访问后台鉴权接口
-//   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-//   console.log('🚀 ~ authMiddleware ~ isApiAuthRoute:', isApiAuthRoute)
-
-//   // 是否是公共路由
-//   const isPublicRoute = routeTester(publicRoutes, nextUrl.pathname)
-//   // const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
-//   console.log('🚀 ~ authMiddleware ~ isPublicRoute:', isPublicRoute)
-
-//   // 是否是授权路由 如登录页 注册页
-//   const isAuthRoute = routeTester(authRoutes, nextUrl.pathname)
-//   console.log('🚀 ~ authMiddleware ~ isAuthRoute:', isAuthRoute)
-
-//   // 1. 如果是后台接口 直接 return null 不做任何处理
-//   if (isApiAuthRoute) {
-//     // ApiAuthRoute 是后台接口 不需要国际化 所以直接 return null 不做任何处理
-//     return
-//   }
-
-//   // 2. 如果是授权路由 如登录页 注册页
-//   if (isAuthRoute) {
-//     // 已登录，直接跳转到默认登录重定向地址
-//     if (isLoggedIn) {
-//       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
-//     }
-//     // 未登录，继续执行 访问登录页 注册页
-//     return intlMiddleware(req)
-//   }
-
-//   // 3. 如果未登录 且 访问鉴权路由 则 跳转到登录页
-//   if (!isLoggedIn && !isPublicRoute) {
-//     // return Response.redirect(new URL(`/api/auth/signin?callbackUrl=${encodeURIComponent(nextUrl.toString())}`, nextUrl))
-//     let callbackUrl = nextUrl.pathname
-//     if (nextUrl.search) {
-//       callbackUrl += nextUrl.search
-//     }
-//     const encodedCallbackUrl = encodeURIComponent(callbackUrl)
-//     return Response.redirect(new URL(`${SIGN_IN_ROUTE}?callbackUrl=${encodedCallbackUrl}`, nextUrl))
-//   }
-
-//   // 4. 如果是公共路由 则交由国际化中间件处理
-//   return intlMiddleware(req)
-// })
-
-// 创建 next-intl 中间件
-const intlMiddleware = createMiddleware({
-  locales,
-  defaultLocale,
-  localePrefix: 'as-needed'
-})
-
-const authMiddleware = auth((req) => {
+export default auth((req) => {
   console.log('🚀 ~ authMiddleware ~ ROUTE:', req.nextUrl.pathname)
   const { nextUrl } = req
+
   // 登录状态
   const isLoggedIn = !!req.auth
   console.log('🚀 ~ authMiddleware ~ isLoggedIn:', isLoggedIn)
+
   // 是否是访问后台鉴权接口
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
   console.log('🚀 ~ authMiddleware ~ isApiAuthRoute:', isApiAuthRoute)
+
   // 是否是公共路由
   const isPublicRoute = routeTester(publicRoutes, nextUrl.pathname)
   // const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
   console.log('🚀 ~ authMiddleware ~ isPublicRoute:', isPublicRoute)
+
   // 是否是授权路由 如登录页 注册页
   const isAuthRoute = routeTester(authRoutes, nextUrl.pathname)
   console.log('🚀 ~ authMiddleware ~ isAuthRoute:', isAuthRoute)
 
-  if (isApiAuthRoute) return
+  // 1. 如果是后台接口 直接 return null 不做任何处理
+  if (isApiAuthRoute) {
+    // ApiAuthRoute 是后台接口 不需要国际化 所以直接 return null 不做任何处理
+    return
+  }
 
+  // 2. 如果是授权路由 如登录页 注册页
   if (isAuthRoute) {
+    // 已登录，直接跳转到默认登录重定向地址
     if (isLoggedIn) {
-      // Redirect logged-in users from auth routes
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
+    // 未登录，继续执行 访问登录页 注册页
     return intlMiddleware(req)
   }
 
+  // 3. 如果未登录 且 访问鉴权路由 则 跳转到登录页
   if (!isLoggedIn && !isPublicRoute) {
-    // Redirect unauthorized users to login for non-public routes
+    // return Response.redirect(new URL(`/api/auth/signin?callbackUrl=${encodeURIComponent(nextUrl.toString())}`, nextUrl))
     let callbackUrl = nextUrl.pathname
     if (nextUrl.search) {
       callbackUrl += nextUrl.search
@@ -117,20 +68,16 @@ const authMiddleware = auth((req) => {
     return Response.redirect(new URL(`${SIGN_IN_ROUTE}?callbackUrl=${encodedCallbackUrl}`, nextUrl))
   }
 
-  if (isLoggedIn) {
-    return intlMiddleware(req) // Apply internationalization for logged-in users
-  }
+  // 4. 如果是公共路由 则交由国际化中间件处理
+  return intlMiddleware(req)
 })
 
-export default function middleware(req: NextRequest) {
-  const { nextUrl } = req
-  const isPublicRoute = routeTester(publicRoutes, nextUrl.pathname)
-  if (isPublicRoute) {
-    return intlMiddleware(req)
-  } else {
-    return (authMiddleware as any)(req)
-  }
-}
+// 创建 next-intl 中间件
+const intlMiddleware = createMiddleware({
+  locales,
+  defaultLocale,
+  localePrefix: 'as-needed'
+})
 
 // 配置匹配器
 export const config = {
