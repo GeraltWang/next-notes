346237a506c279cf209d383d7bbd0a7bd98be7c7
GeraltWang <wgeralt@outlook.com>
2024-08-30 17:12:21 +0800

    fix: 🐛 sign out redirect with intl

--- lib/actions/sign-out.ts
+++ lib/actions/sign-out.ts
@@ -1,10 +1,12 @@
 'use server'
 import { SIGN_IN_ROUTE } from '@/routes'
 import { signOut } from 'auth'
+import { redirect } from '@/routing'
 
 export const signOutAction = async () => {
   await signOut({
-    redirectTo: SIGN_IN_ROUTE,
-    redirect: true
+    // redirectTo: SIGN_IN_ROUTE,
+    redirect: false
   })
+  redirect(SIGN_IN_ROUTE)
 }
