c0d04931ed12e876eec106bb67286594ecc49e09
GeraltWang <wgeralt@outlook.com>
2024-08-30 15:51:11 +0800

    ci: 👷 middleware and dep

--- middleware.ts
+++ middleware.ts
@@ -13,49 +13,38 @@ const intlMiddleware = createMiddleware({
   localePrefix: 'always'
 })
 
-const authMiddleware = auth(async (req) => {
+const authMiddleware = auth((req) => {
   const { nextUrl } = req
   const isLoggedIn = !!req.auth
   const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
-  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  // const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
   const isAuthRoute = authRoutes.includes(nextUrl.pathname)
 
   // Handle different route scenarios
   if (isApiAuthRoute) return // Don't modify API authentication routes
 
-  if (isAuthRoute) {
-    if (isLoggedIn) {
-      // Redirect logged-in users from auth routes
-      return NextResponse.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+  if (isLoggedIn) {
+    if (isAuthRoute) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
     }
-    return // Don't modify behavior for auth routes
+    return intlMiddleware(req)
+  } else {
+    return Response.redirect(new URL(SIGN_IN_ROUTE, nextUrl))
   }
-
-  if (!isLoggedIn && !isPublicRoute) {
-    // Redirect unauthorized users to login for non-public routes
-    return NextResponse.redirect(new URL(SIGN_IN_ROUTE, nextUrl))
-  }
-
-  return NextResponse.next()
 })
 
-export default async function middleware(req: NextRequest) {
+export default function middleware(req: NextRequest) {
   const publicPathnameRegex = RegExp(
-    `^(/(${locales.join('|')}))?(${publicRoutes.flatMap((p) => (p === '/' ? ['', '/'] : p)).join('|')})/?$`,
+    `^(/(<span class="math-inline">\{locales\.join\("\|"\)\}\)\)?\(</span>{publicPages
+      .flatMap((p) => (p === "/" ? ["", "/"] : p))
+      .join("|")})/?$`,
     'i'
   )
 
   const isPublicPage = publicPathnameRegex.test(req.nextUrl.pathname)
 
-  // First apply internationalization middleware
-  const intlResponse = intlMiddleware(req)
-  if (intlResponse) {
-    return intlResponse
-  }
-
-  // Then apply authentication middleware
   if (isPublicPage) {
-    return NextResponse.next() // Allow access to public pages
+    return intlMiddleware(req) // Apply internationalization for public pages
   } else {
     return (authMiddleware as any)(req) // Apply authentication logic for non-public pages
   }
